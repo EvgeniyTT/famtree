5501d42bd7b9d2044ce7e8930da9c8d47e9fa4d0
Yevhen Tataryn <tatarin.e.m@gmail.com>
2019-02-13 23:21:12 +0100

    added Leaf andDetails as separate components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-import { data } from './data';
+import Leaf from './components/Leaf';
+import PersonDetails from './components/DetailsSection';
 
 const styles = theme => ({
   root: {
@@ -19,30 +15,18 @@ const styles = theme => ({
   },
 });
 
-function SimpleExpansionPanel(props) {
+function App(props) {
   const { classes } = props;
   return (
     <div className={classes.root}>
-      {data.map(data=>
-        <ExpansionPanel>
-          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography className={classes.heading}>My age is - {data.age}</Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-              sit amet blandit leo lobortis eget.
-            </Typography>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-      )}
-
+      <Leaf />
+      <PersonDetails />
     </div>
   );
 }
 
-SimpleExpansionPanel.propTypes = {
+App.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(SimpleExpansionPanel);
+export default withStyles(styles)(App);
---
+++ src/components/DetailsSection/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+import { data } from '../../data';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+});
+
+function SimpleExpansionPanel(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      {data.map(data=>
+        <ExpansionPanel>
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography className={classes.heading}>My age is - {data.age}</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+              {Object
+                .keys(data)
+                .map(prop => 
+                  (<div key={prop}>
+                    <span className="title">{prop}:</span>
+                    <span className="value">{data[prop]}</span>
+                  </div>)
+                )
+              }
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      )}
+
+    </div>
+  );
+}
+
+SimpleExpansionPanel.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SimpleExpansionPanel);
