ca6e84837186423eed190bffde62a6d389d284b7
Yevhen Tataryn <tatarin.e.m@gmail.com>
2019-02-14 22:25:20 +0100

    Updates

---
+++ precache-manifest.3945aa20a970dd8ae78767f3d873cf03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c25f65ac9d8d883c23e",
+    "url": "/famtree/static/css/main.060e00b1.chunk.css"
+  },
+  {
+    "revision": "9c25f65ac9d8d883c23e",
+    "url": "/famtree/static/js/main.9c25f65a.chunk.js"
+  },
+  {
+    "revision": "638b8ae5aaa9d95413ee",
+    "url": "/famtree/static/js/1.638b8ae5.chunk.js"
+  },
+  {
+    "revision": "62bbf71ff4d31b9660c4",
+    "url": "/famtree/static/js/runtime~main.62bbf71f.js"
+  },
+  {
+    "revision": "4857c3a79c8cce50f5f0944ada75fe06",
+    "url": "/famtree/static/media/anna.4857c3a7.jpg"
+  },
+  {
+    "revision": "e2a2a943dd4f223b701cb8702882173f",
+    "url": "/famtree/index.html"
+  }
+];
